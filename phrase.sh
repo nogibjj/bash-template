a8b258f08bbcca70fe01a34b1e3cd63966509c31
Noah Gift <noah.gift@gmail.com>
2022-11-15 18:51:19 +0000

    adding phrase

--- phrase.sh
+++ phrase.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
+#example: ./phrase.sh -c 5 -p "Hello World"
+
+#if the user does not enter any arguments, print the usage
+#basic error handling
+#create a help function with -h and --help
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 -c <count> -p <phrase>"
+    exit 1
+fi
+
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 -c <count> -p <phrase>"
+    exit 1
+fi
+
+#print help
+if [ "$1" == "-h"]; then
+    echo "Usage: ./phrase.sh -c <number of times to print> -p <phrase to print>"
+    exit 0
+fi
+
 
 #Generate phrase "N" times
 phrase_generator() {
@@ -12,6 +33,7 @@ while [[ $# -gt 1 ]]
 do
 key="$1"
 
+#add help option
 case $key in
     -c|--count)
     COUNT="$2"
@@ -26,5 +48,4 @@ shift
 done
 
 #Run program
-#example: ./phrase.sh -c 5 -p "Hello World"
 phrase_generator "${COUNT}" "${PHRASE}"
